70e1b093968bd0b09faac0579eb0a156c54455e0
deepaknlp <deepak.gupta651@gmail.com>
2023-05-23 17:07:52 -0400

    Update sanity-check.py

--- Task 1/sanity-check.py
+++ Task 1/sanity-check.py
@@ -8,25 +8,72 @@ def read_json(data_path):
 		data_items = json.load(rfile)
 	return data_items
 
-	
-def check_fields(data_path):
+
+def check_fields(data_path, question_size=2):
 	#### check the json file contains the required fields
 	data_items = read_json(data_path)
+	if len(data_items)!=question_size:
+		print(f"submitted prediction json file doesn't have predictions for all {question_size} questions!")
+		sys.exit(0)
+
 	for data_item in data_items:
-		if 'video_id' not in data_item:
-			print(f"submitted prediction json file doesn't have required 'valid_id' field for {data_item}!")
-			sys.exit(0)
-		if 'label' not in data_item:
-			print(f"submitted prediction json file doesn't have required 'label' field for {data_item}!")
-			sys.exit(0)
-		
-		if data_item['video_id']=='' or data_item['video_id']==None:
-			print("'valid_id' field can not be empty")
+		if 'question_id' not in data_item:
+			print(f"submitted prediction json file doesn't have required 'question_id' field for {data_item}!")
 			sys.exit(0)
 
-		if data_item['label']=='' or data_item['label']==None:
-			print("'label' field can not be empty")
+		if data_item['question_id']=='' or data_item['question_id']==None:
+			print(f"submitted prediction json file have empty 'question_id' field for {data_item}")
 			sys.exit(0)
+
+		if 'relevant_videos' not in data_item:
+			print(f"submitted prediction json file doesn't have required 'relevant_videos' field for {data_item}!")
+			sys.exit(0)
+
+		if 'relevant_videos' in data_item and type(data_item['relevant_videos']) is not list:
+			print(f"submitted prediction json file doesn't have 'relevant_videos' list for {data_item}!")
+			sys.exit(0)
+
+		if len(data_item['relevant_videos'])==0:
+			print(f"submitted prediction json file have zero 'relevant_videos' for questiond id {data_item['question_id']}!")
+			sys.exit(0)
+		
+		for rel_vid_item in data_item['relevant_videos']:
+			if type(rel_vid_item) is not dict:
+				print(f"submitted prediction json file have following issue: the data type is not dictionary for one of the 'relevant_videos' related to questiond id {data_item['question_id']}!")
+				sys.exit(0)
+
+			if 'video_id' not in rel_vid_item:
+				print(f"submitted prediction json file doesn't have required 'video_id' field for questiond id {data_item['question_id']}!")
+				sys.exit(0)
+			
+			if rel_vid_item['video_id']=='' or rel_vid_item['video_id']==None:
+				print("'video_id' field can not be empty")
+				sys.exit(0)
+
+
+			if 'answer_start_second' not in rel_vid_item:
+				print(f"submitted prediction json file doesn't have required 'answer_start_second' field for questiond id {data_item['question_id']} and relevant_video {rel_vid_item['video_id']}!")
+				sys.exit(0)
+
+			if 'answer_end_second' not in rel_vid_item:
+				print(f"submitted prediction json file doesn't have required 'answer_end_second' field for questiond id {data_item['question_id']} and relevant_video {rel_vid_item['video_id']}!")
+				sys.exit(0)
+		
+		
+			if rel_vid_item['answer_start_second']=='' or rel_vid_item['answer_start_second']==None:
+				print("'answer_start_second' field can not be empty")
+				sys.exit(0)
+
+			if rel_vid_item['answer_end_second']=='' or rel_vid_item['answer_end_second']==None:
+				print("'answer_start_second' field can not be empty")
+				sys.exit(0)
+
+			if type(rel_vid_item['answer_start_second']) not in [float, int] or type(rel_vid_item['answer_end_second']) not in [float, int]:
+				print("'answer_start_second'/'answer_end_second' field must be int/float value!")
+				sys.exit(0)
+
+
+
 
 def check_file_name_and_type(submission_file):
 	### check the file name
